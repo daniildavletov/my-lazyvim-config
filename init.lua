74b251ec72b943d6caa9a2d3890f78257c91a558
Daniil Davletov <d.daniil.d@gmail.com>
2025-11-05 19:12:14 +0500

    initial commit

---
+++ init.lua
@@ -0,0 +1,23 @@
+require("config.lazy")
+
+vim.g.clipboard = {
+  name = "WslClipboard",
+  copy = {
+    ["+"] = { "clip.exe" },
+    ["*"] = { "clip.exe" },
+  },
+  paste = {
+    ["+"] = {
+      "/mnt/c/Windows/System32/WindowsPowerShell/v1.0///powershell.exe",
+      "-c",
+      '[Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+    },
+    ["*"] = {
+      "/mnt/c/Windows/System32/WindowsPowerShell/v1.0///powershell.exe",
+      "-c",
+      '[Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+    },
+  },
+
+  cache_enabled = false,
+}
